669422e9e04bd6767f61f54d8e9dd6f12418a3ba
Gianluca Borello <g.borello@gmail.com>
2015-05-16 18:58:25 -0700

    More tests

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -5,7 +5,23 @@ TESTDIR="$(dirname "$(readlink -f "$0")")"
 APPDIR=$TESTDIR/..
 
 python $APPDIR/cassandradump.py --import-file $TESTDIR/test_keyspace_init.cql
-python $APPDIR/cassandradump.py --export-file /tmp/export1.cql --keyspace test_keyspace
-python $APPDIR/cassandradump.py --import-file /tmp/export1.cql
-python $APPDIR/cassandradump.py --export-file /tmp/export2.cql --keyspace test_keyspace
-diff /tmp/export1.cql /tmp/export2.cql
+
+python $APPDIR/cassandradump.py --export-file /tmp/dump.cql --keyspace test_keyspace
+diff $TESTDIR/full_export.cql.expected /tmp/dump.cql
+
+python $APPDIR/cassandradump.py --import-file $TESTDIR/full_export.cql.expected
+
+python $APPDIR/cassandradump.py --export-file /tmp/dump.cql --keyspace test_keyspace
+diff $TESTDIR/full_export.cql.expected /tmp/dump.cql
+
+python $APPDIR/cassandradump.py --export-file /tmp/dump.cql --cf test_keyspace.twenty_rows_composite_table
+diff $TESTDIR/cf_export.cql.expected /tmp/dump.cql
+
+python $APPDIR/cassandradump.py --export-file /tmp/dump.cql --cf test_keyspace.twenty_rows_composite_table --no-create
+diff $TESTDIR/cf_export_nocreate.cql.expected /tmp/dump.cql
+
+python $APPDIR/cassandradump.py --export-file /tmp/dump.cql --cf test_keyspace.twenty_rows_composite_table --no-insert
+diff $TESTDIR/cf_export_noinsert.cql.expected /tmp/dump.cql
+
+python $APPDIR/cassandradump.py --export-file /tmp/dump.cql --filter "test_keyspace.twenty_rows_composite_table WHERE a = 'A' AND b = '19'"
+diff $TESTDIR/filter.cql.expected /tmp/dump.cql
